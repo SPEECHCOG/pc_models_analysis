b7b16ef035111c54f2e302ec18169b501f5909ea
orasanen <okko.rasanen@tuni.fi>
2020-02-14 17:24:19 +0200

    flexibility

--- synthesizeTimeStamps.m
+++ synthesizeTimeStamps.m
@@ -1,34 +1,76 @@
-function ZS = synthesizeTimeStamps(ZS,ws)
-% function ZS = synthesizeTimeStamps(ZS,ws)
+function ZS = synthesizeTimeStamps(ZS,ws,offset)
+% function ZS = synthesizeTimeStamps(ZS,ws,offset)
 % 
 % Creates equal spacing timestamps for all extracted features with frame
 % shift ws.
+
+if nargin <3
+    offset = 0;
+end
 
 languages = fields(ZS);
 
 for langiter = 1:length(languages)
    if(isfield(ZS.(languages{1}).train,'features'))
        ff = ZS.(languages{1}).train.features;
-       tt = createTimeStamps(ff,ws);
+       tt = createTimeStamps(ff,ws,offset);
+       
+       for k = 1:length(tt)
+           tmp = tt{k} < 0;
+           ZS.(languages{1}).train.features{k}(tmp,:) = [];
+           tt{k}(tmp) = [];           
+       end
+       
        ZS.(languages{1}).train.features_t = tt;
    end
    
    if(isfield(ZS.(languages{1}).test,'features_1'))
        ff = ZS.(languages{1}).test.features_1;
-       tt = createTimeStamps(ff,ws);
+       tt = createTimeStamps(ff,ws,offset);
+       
+           
+       for k = 1:length(tt)
+           tmp = tt{k} < 0;
+           tmp = tmp+tt{k} > 1;                      
+           ZS.(languages{1}).test.features_1{k}(tmp,:) = [];
+           tt{k}(tmp) = [];           
+       end
+       
        ZS.(languages{1}).test.features_1_t = tt;
+       
+       
    end
    
    if(isfield(ZS.(languages{1}).test,'features_10'))
        ff = ZS.(languages{1}).test.features_10;
-       tt = createTimeStamps(ff,ws);
+       tt = createTimeStamps(ff,ws,offset);
+              
+       for k = 1:length(tt)
+           tmp = tt{k} < 0;          
+           tmp = tmp+tt{k} > 10;                      
+           ZS.(languages{1}).test.features_10{k}(tmp,:) = [];
+           tt{k}(tmp) = [];           
+       end
+       
        ZS.(languages{1}).test.features_10_t = tt;
+       
    end
    
    if(isfield(ZS.(languages{1}).test,'features_120'))
        ff = ZS.(languages{1}).test.features_120;
-       tt = createTimeStamps(ff,ws);
-       ZS.(languages{1}).test.features_120_t = tt;
+       tt = createTimeStamps(ff,ws,offset);       
+       
+       for k = 1:length(tt)
+           
+           
+           tmp = tt{k} < 0;
+           tmp = tmp+tt{k} > 120;                      
+           ZS.(languages{1}).test.features_120{k}(tmp,:) = [];
+           tt{k}(tmp) = [];           
+       end
+       
+       ZS.(languages{1}).test.features_120_t = tt;       
+       
    end
    
     
@@ -36,7 +78,7 @@ for langiter = 1:length(languages)
 end
 
 
-function tt = createTimeStamps(ff,ws)
+function tt = createTimeStamps(ff,ws,offset)
 
 tt = cell(length(ff),1);
 for k = 1:length(ff)
@@ -44,6 +86,6 @@ for k = 1:length(ff)
     
     t = ws/2:ws:len*ws-ws/2;
     
-    tt{k} = t;
+    tt{k} = t-offset;
     
 end
