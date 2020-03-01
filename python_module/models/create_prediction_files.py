Fix bugs related to precion, calculation of total samples, and testing code deleted. The evaluation task although it is affected by the precision in printed numbers does not drastically change, therefore precision of 5 decimals is being used now. Similarly, the effect normalisation of the whole test/train set has a very small (2.1209e-5) in the across metric but 0 in the within metric, That normalisation could be removed if features are normalised consistently when they are calculated. The latest mean Mel features need to be normalised in the extraction. Regarding the calculation of samples, for training we can trained without all the frames if the division total_fames/frames_per_sample is a decimal, however, in the case of testing set, we need to include all the frames otherwise last files will miss the last frames. In that sense, we need to pad the reminder (of the division, to make it into a sample) to waranty we will predict all files completed.

python_module/models/create_prediction_files.py
@@ -7,8 +7,6 @@
 import os
 import numpy as np
 
-from astropy.io import ascii
-
 
 def calculate_timestamps(n_frames, window_shift):
     """
@@ -34,13 +32,7 @@ def create_output_file(features, file_path, window_shift):
     timestamps = calculate_timestamps(n_frames, window_shift)
 
     # save txt file
-    # np.savetxt(file_path, np.c_[timestamps, features], delimiter=' ', fmt='%s')
-    feats = np.c_[timestamps, features]
-
-    formats = {'col0': '%.6f'}
-    for j in range(features.shape[1]):
-        formats['col'+str(j+1)] = '%.6f'
-    ascii.write(feats, file_path, format='no_header', delimiter=' ', formats=formats, overwrite=True)
+    np.savetxt(file_path, np.c_[timestamps, features], delimiter=' ', fmt='%.5f')
 
 
 def create_prediction_files(prediction, indices, folder_path, window_shift, limit=None):